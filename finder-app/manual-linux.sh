0e807606aff08e98c4a17f1f78c55c070f31af51
Renato Soma <renatoys08@gmail.com>
2023-07-14 18:50:35 -0300

    fix arg

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,9 +12,6 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-SYS_FS=~/Documents/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
-ROOT_FS=~/Documents/a3_part2/rootfs
-ASSIGNMENT_PATH=~/Documents/assignment-1-rntsoma
 
 if [ $# -lt 1 ]
 then
@@ -25,6 +22,8 @@ else
 fi
 
 mkdir -p ${OUTDIR}
+mkdir -p ${OUTDIR}/rootfs
+ROOT_FS=${OUTDIR}/rootfs
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -83,7 +82,7 @@ fi
 
 # TODO [Done]: Make and install busybox
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=/home/rys/Documents/a3_part2/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 mkdir bin
 cp busybox bin/
 
@@ -92,10 +91,10 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO [Done]: Add library dependencies to rootfs
-cp `find $SYS_FS -name ld-linux-aarch64.so.1` $ROOT_FS/lib
-cp `find $SYS_FS -name libm.so.6` $ROOT_FS/lib64
-cp `find $SYS_FS -name libresolv.so.2` $ROOT_FS/lib64
-cp `find $SYS_FS -name libc.so.6` $ROOT_FS/lib64
+cp ${FINDER_APP_DIR}/ld-linux-aarch64.so.1 $ROOT_FS/lib
+cp ${FINDER_APP_DIR}/libm.so.6 $ROOT_FS/lib64
+cp ${FINDER_APP_DIR}/libresolv.so.2 $ROOT_FS/lib64
+cp ${FINDER_APP_DIR}/libc.so.6 $ROOT_FS/lib64
 
 # TODO [Done]: Make device nodes
 cd "$OUTDIR"
@@ -103,8 +102,10 @@ sudo mknod -m 666 rootfs/dev/null c 1 3
 sudo mknod -m 666 rootfs/dev/tty c 5 1
 
 # TODO [Done]: Clean and build the writer utility
-cd $ASSIGNMENT_PATH/finder-app
-make clean
+cd ${FINDER_APP_DIR}
+if [ -e "./writer" ]; then
+    make clean
+fi
 make CROSS_COMPILE=aarch64-none-linux-gnu-
 
 # TODO [Done]: Copy the finder related scripts and executables to the /home directory
@@ -126,3 +127,5 @@ sudo chown -R root:root $ROOT_FS/../rootfs
 cd $OUTDIR/rootfs
 find . | cpio -H newc -ov --owner root:root > $OUTDIR/initramfs.cpio
 gzip -f $OUTDIR/initramfs.cpio
+
+rm -rf $OUTDIR/busybox/bin
